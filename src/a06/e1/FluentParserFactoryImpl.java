e19f91f487f1d60fcada54a568ce7dc6f4ffb4e0
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2024-12-16 21:43:01 +0100

    createList method now use stream instead of foreach

--- src/a06/e1/FluentParserFactoryImpl.java
+++ src/a06/e1/FluentParserFactoryImpl.java
@@ -1,8 +1,6 @@
 package a06.e1;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.UnaryOperator;
@@ -32,7 +30,7 @@ public class FluentParserFactoryImpl implements FluentParserFactory {
     @Override
     public FluentParser<List<Integer>> incrementalNaturalLists() {
         return new FluentParserImpl<List<Integer>>(Stream.iterate(
-                (List<Integer>) new ArrayList<Integer>(),
+                (List<Integer>) new ArrayList<Integer>(), //can't find a way to avoid using cast
                 list -> {
                     List<Integer> newList = new ArrayList<>(list);
                     newList.add(newList.size());
@@ -43,7 +41,7 @@ public class FluentParserFactoryImpl implements FluentParserFactory {
 
     @Override
     public FluentParser<Integer> repetitiveIncrementalNaturals() {
-        return new FluentParserImpl<>(IntStream.iterate(0, i -> i + 1).flatMap(gruop ->  IntStream.rangeClosed(0, gruop)).iterator());
+        return new FluentParserImpl<>(IntStream.iterate(0, i -> i + 1).flatMap(group ->  IntStream.rangeClosed(0, group)).iterator());
     }
 
     @Override
@@ -51,17 +49,13 @@ public class FluentParserFactoryImpl implements FluentParserFactory {
         return new FluentParserImpl<>(Stream.iterate(1, i -> i + 1).flatMap(group -> IntStream.rangeClosed(1, group).mapToObj(n -> s.repeat(n))).iterator());    
     }
 
-    private List<String> createList(int size, String s) {
-        List<String> toReturn = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            toReturn.add(s);
-        }
-        return toReturn;
+    private List<String> createList(String s, int size) {
+        return IntStream.range(0, size).mapToObj(i -> s).toList();
     }
 
     @Override
     public FluentParser<Pair<Integer, List<String>>> incrementalPairs(int i0, UnaryOperator<Integer> op, String s) {
-        return new FluentParserImpl<>(Stream.iterate(i0, op).map(i -> new Pair<>(i, createList(i, s))).iterator());
+        return new FluentParserImpl<>(Stream.iterate(i0, op).map(i -> new Pair<>(i, createList(s, i))).iterator());
     }
 
 }
